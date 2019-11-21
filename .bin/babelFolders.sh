411ea46ad378fc167a34d9bcc5efac4227e92504
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-11-21 13:10:06 -0300

    fixed: correct babel compiler

--- .bin/babelFolders.sh
+++ .bin/babelFolders.sh
@@ -1,25 +1,18 @@
 #!/bin/dash
 compileDir () {
     for d in $1; do
-        echo $d
         case $d in 
             *.ts)
-                # $a="publish"
-                # echo $d
+                echo $d
                 a="${d//'src'/'publish'}"
                 a=${a//".ts"/'.js'}
                 ./node_modules/.bin/babel $d --out-file $a 
-                # echo $a
                 ;;
             *)
-                echo $d
                 compileDir "$d/*"
                 ;;
         esac
     done
 }
-# echo $0
+
 compileDir "./src/*"
-
-
-# ../src/*
