d9c945caf1bac1dfff7a7b60c311b80f59b83b55
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-11-21 09:25:46 -0300

    fixed: babel multiple file compiler for linux

--- .bin/babelFolders.sh
+++ .bin/babelFolders.sh
@@ -1,6 +1,4 @@
 #!/bin/dash
-
-
 compileDir () {
     for d in $1; do
         echo $d
@@ -10,7 +8,7 @@ compileDir () {
                 # echo $d
                 a="${d//'src'/'publish'}"
                 a=${a//".ts"/'.js'}
-                ../node_modules/.bin/babel $d --out-file $a 
+                ./node_modules/.bin/babel $d --out-file $a 
                 # echo $a
                 ;;
             *)
@@ -20,8 +18,8 @@ compileDir () {
         esac
     done
 }
-
-compileDir "../src/*"
+# echo $0
+compileDir "./src/*"
 
 
 # ../src/*
