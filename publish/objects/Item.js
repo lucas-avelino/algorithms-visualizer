f4a5fce16063a766ab80f215053f1fbb47115c23
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-11-21 13:51:39 -0300

    removed: published files

--- publish/objects/Item.js
+++
@@ -1,7 +0,0 @@
-export default class Item {
-  constructor(id, value) {
-    this.id = id;
-    this.value = value;
-  }
-
-}
--- publish/objects/Sorter.js
+++
@@ -1,8 +0,0 @@
-export default class SorterConfig {
-  constructor(id, label, functionName) {
-    this.id = id;
-    this.label = label;
-    this.functionName = functionName;
-  }
-
-}
