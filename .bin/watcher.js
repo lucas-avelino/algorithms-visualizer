305fccad0bd430573be1981cf4ea8aacac2a56c7
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-12-10 11:37:58 -0300

    added: new watcher for auto build files in DEV

---
+++ .bin/watcher.js
@@ -0,0 +1,16 @@
+
+var watch = require('node-watch');
+const { exec } = require('child_process');
+watch('src/', { recursive: true }, function(evt, name) {
+  console.log('%s changed.', name);
+  nameChanged = name.replace("src/","publish/").replace(".ts",".js");
+  exec(`node_modules/.bin/babel ${name} --out-file ${nameChanged} --extensions`,(err, stdout, stderr) => {
+    //./node_modules/.bin/babel ${name} --out-file ${nameChanged} --extensions
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  })
+});
